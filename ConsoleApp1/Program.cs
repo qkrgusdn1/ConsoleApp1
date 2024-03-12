a8bf092b3b7b56e3fb23661c1b069ecd76605105
qkrgusdn1 <163072109+qkrgusdn1@users.noreply.github.com>
2024-03-12 11:18:09 +0900

    GiftBox 추가
    Letter와 Money를 넣고 배송기사님도 넣어줬다 그리고 public도 써보고
    쓰고싶은말

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace ConsoleApp1
+﻿using System.Data.SqlTypes;
+
+namespace ConsoleApp1
 {
+    class GiftBox
+    {
+        public string? Letter;
+        public int Money;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GiftBox a = new GiftBox()
+            {
+                Letter = "잘 지내지?",
+                Money = 100000
+            };
+            Console.WriteLine("A송장내용");
+            Console.WriteLine(a.Letter);
+            Console.WriteLine(a.Money);
+            GiftBox 배송기사님 = a;
+
+            Console.WriteLine("배송기사님 송장내용");
+            Console.WriteLine(배송기사님.Letter);
+            Console.WriteLine(배송기사님.Money);
+
+            a.Letter = "어떻게 지내고 있어?";
+            a.Money = 150000;
+
+            Console.WriteLine("A 변경 이루 송장내용");
+            Console.WriteLine(a.Letter);
+            Console.WriteLine(a.Money);
+
+            Console.WriteLine("배송기사님 송장내용");
+            Console.WriteLine(배송기사님.Letter);
+            Console.WriteLine(배송기사님.Money);
         }
     }
 }
